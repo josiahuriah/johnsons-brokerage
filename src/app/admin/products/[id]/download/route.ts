b2947c454663abe72f242c8b069cc21b631b5f69
Josh <josh.duncanson@gmail.com>
2025-10-01 07:10:29 -0400

    Made dynamic route fixes

--- src/app/admin/products/[id]/download/route.ts
+++ src/app/admin/products/[id]/download/route.ts
@@ -1,29 +1,30 @@
 import db from "@/db/db"
-import { notFound } from "next/navigation"
 import { NextRequest, NextResponse } from "next/server"
 import fs from "fs/promises"
 
 export async function GET(
-    req: NextRequest, 
-    { params }: { params: Promise<{ id: string }> }
+  req: NextRequest,
+  { params }: { params: Promise<{ downloadVerificationId: string }> }
 ) {
-    const { id } = await params
-    
-    const product = await db.product.findUnique({
-        where: { id }, 
-        select: { filePath: true, name: true }
-    })
+  const { downloadVerificationId } = await params
+  
+  const data = await db.downloadVerification.findUnique({
+    where: { id: downloadVerificationId, expiresAt: { gt: new Date() } },
+    select: { product: { select: { filePath: true, name: true } } },
+  })
 
-    if (product == null) return notFound()
+  if (data == null) {
+    return NextResponse.redirect(new URL("/products/download/expired", req.url))
+  }
 
-    const { size } = await fs.stat(product.filePath)
-    const file = await fs.readFile(product.filePath)
-    const extension = product.filePath.split(".").pop()
+  const { size } = await fs.stat(data.product.filePath)
+  const file = await fs.readFile(data.product.filePath)
+  const extension = data.product.filePath.split(".").pop()
 
-    return new NextResponse(file, {
-        headers: {
-            "Content-Disposition": `attachment; filename="${product.name}.${extension}"`,
-            "Content-Length": size.toString()
-        }
-    })
+  return new NextResponse(file, {
+    headers: {
+      "Content-Disposition": `attachment; filename="${data.product.name}.${extension}"`,
+      "Content-Length": size.toString(),
+    },
+  })
 }
