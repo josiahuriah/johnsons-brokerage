1074c239d1ac3206c919331dc72a23fcf8964c60
Josh <josh.duncanson@gmail.com>
2025-10-02 03:36:19 -0400

    Fixed type error

--- src/app/(customerFacing)/products/page.tsx
+++ src/app/(customerFacing)/products/page.tsx
@@ -2,30 +2,37 @@ import { ProductCard, ProductCardSkeleton } from "@/components/ProductCard";
 import { Suspense } from "react";
 import db from "@/db/db"
 import { cache } from "@/lib/cache";
+import { Product } from "@/generated/prisma";
 
 const getProducts = cache(() => {
-    return db.product.findMany({where: {
-        isAvailableForPurchase: true}, 
-        orderBy: { name: "asc"},})
+    return db.product.findMany({
+        where: {
+            isAvailableForPurchase: true
+        }, 
+        orderBy: { name: "asc" }
+    })
 }, ["/products", "getProducts"])
 
 export default function ProductsPage() {
     return (
-    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
-        <Suspense fallback={ <>
-            < ProductCardSkeleton/>
-            < ProductCardSkeleton/>
-            < ProductCardSkeleton/>
-        </>} >
-        <ProductsSuspense  />
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+            <Suspense fallback={
+                <>
+                    <ProductCardSkeleton/>
+                    <ProductCardSkeleton/>
+                    <ProductCardSkeleton/>
+                </>
+            }>
+                <ProductsSuspense />
             </Suspense>
-    </div>
+        </div>
     )
 }
 
 async function ProductsSuspense() {
     const products = await getProducts()
     
-    return products.map(product => 
-    <ProductCard key={product.id} {...product} /> )
+    return products.map((product: Product) => 
+        <ProductCard key={product.id} {...product} />
+    )
 }
